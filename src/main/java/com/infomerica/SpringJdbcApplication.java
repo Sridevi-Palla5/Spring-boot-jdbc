66f49b808f6356e3f6ac5b6fc1c88b29af68cad0
Sridevi_Palla <sridevi.palla@infomericainc.in>
2025-03-14 09:56:48 +0530

    Added Log for Main Class

--- src/main/java/com/infomerica/SpringJdbcApplication.java
+++ src/main/java/com/infomerica/SpringJdbcApplication.java
@@ -3,10 +3,14 @@ package com.infomerica;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class SpringJdbcApplication {
 
 	public static void main(String[] args) {
+		log.info("SpringJdbcApplication Started ................................");
 		SpringApplication.run(SpringJdbcApplication.class, args);
 	}
 
